ba204f7fa70db06dbceacfd9ddfd67a2c1edefe8
IncasatorM <mkaminskyi97@gmail.com>
2023-06-21 16:57:54 +0300

    Homework 16 Mark

---
+++ Src/Program.java
@@ -0,0 +1,13 @@
+
+public class Program {
+    public static void main(String[] args) {
+        User user = new User("Alice", "qwerty");
+        user.displayAccount("qwerty");
+
+            
+
+
+
+
+    }
+}
---
+++ Src/User.java
@@ -0,0 +1,49 @@
+public class User {
+    private String login;
+
+    Acount acount;
+
+
+    public User(String login, String password) {
+        this.login = login;
+        acount = new Acount(password);
+
+    }
+
+    public User(String login) {
+        this.login = login;
+    }
+
+    class Acount {
+        private String password;
+
+        private Acount(String password) {
+            this.password = password;
+        }
+    }
+
+    public void displayAccount(String pass) {
+        class Account {
+            String password;
+
+            Account() {
+                password = pass;
+            }
+
+            void display() {
+                System.out.printf("Account Login: %s \t Password: %s \n", login, password);
+            }
+        }
+        Account account = new Account();
+        account.display();
+
+
+    }
+
+
+}
+
+
+
+
+
